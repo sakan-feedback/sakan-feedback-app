ab22c7a87a902593be09974b56b8f167d3f6afc4
sakan-feedback <sakanamna6@gmail.com>
2026-04-06 11:54:57 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", (req, res) => {
+  res.send("SAKAN Feedback System is Running ✅");
+});
+
+app.post("/feedback", async (req, res) => {
+  const { name, location, comments } = req.body;
+
+  await pool.query(
+    "INSERT INTO feedback (name, location, comments) VALUES ($1, $2, $3)",
+    [name, location, comments]
+  );
+
+  res.json({ message: "Saved" });
+});
+
+app.get("/feedback", async (req, res) => {
+  const result = await pool.query("SELECT * FROM feedback ORDER BY id DESC");
+  res.json(result.rows);
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
